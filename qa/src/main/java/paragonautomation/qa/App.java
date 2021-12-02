f309544a07c85b0b3a23d942c39c6c9e42101082
shatrughan.singh <shatrughan.singh@LPCL-PG0225M9.espire.com>
2021-12-02 16:27:22 +0530

    commit changes

---
+++ qa/src/main/java/paragonautomation/qa/App.java
@@ -0,0 +1,13 @@
+package paragonautomation.qa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
